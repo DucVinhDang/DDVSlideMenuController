200c974c18868772fe3fec4eb73a4565b5da69d7
Vinh Dang <ducvinh0812@gmail.com>
2015-06-13 18:15:15 +0700

    Edit something

--- DDVSlideMenuControllerExample/DDVSlideMenuController.swift
+++ DDVSlideMenuControllerExample/DDVSlideMenuController.swift
@@ -60,7 +60,7 @@ class DDVSlideMenuController: UIViewController {
     
     let deviceWidth = UIScreen.mainScreen().bounds.width
     let deviceHeight = UIScreen.mainScreen().bounds.height
-    let distanceOffSet: CGFloat = 80
+    let distanceOffSet: CGFloat = 70
     let timeSliding: NSTimeInterval = 0.5
     let shadowOpacity: Float = 0.8
     
@@ -178,19 +178,19 @@ class DDVSlideMenuController: UIViewController {
                 }
             }
         case .Changed:
-            if(slidePanelState == .Left) {
-                if CGRectGetMinX(centerViewController.view.frame) > (deviceWidth - distanceOffSet) {
-                    centerViewController.view.frame.origin.x = (deviceWidth - distanceOffSet)
-                } else if CGRectGetMinX(centerViewController.view.frame) < 0 {
-                    centerViewController.view.frame.origin.x = 0
-                }
-                centerViewController.view.center.x = centerViewController.view.center.x + gesture.translationInView(view).x
-                gesture.setTranslation(CGPointZero, inView: view)
-            } else if(slidePanelState == .Right) {
-                if CGRectGetMaxX(centerViewController.view.frame) < distanceOffSet {
-                    centerViewController.view.frame.origin.x = distanceOffSet - centerViewController.view.bounds.width
-                } else if CGRectGetMinX(centerViewController.view.frame) > 0 {
-                    centerViewController.view.frame.origin.x = 0
+            if slidePanelState != .None {
+                if(slidePanelState == .Left) {
+                    if CGRectGetMinX(centerViewController.view.frame) > (deviceWidth - distanceOffSet) {
+                        centerViewController.view.frame.origin.x = (deviceWidth - distanceOffSet)
+                    } else if CGRectGetMinX(centerViewController.view.frame) < 0 {
+                        centerViewController.view.frame.origin.x = 0
+                    }
+                } else if(slidePanelState == .Right) {
+                    if CGRectGetMaxX(centerViewController.view.frame) < distanceOffSet {
+                        centerViewController.view.frame.origin.x = distanceOffSet - centerViewController.view.bounds.width
+                    } else if CGRectGetMinX(centerViewController.view.frame) > 0 {
+                        centerViewController.view.frame.origin.x = 0
+                    }
                 }
                 centerViewController.view.center.x = centerViewController.view.center.x + gesture.translationInView(view).x
                 gesture.setTranslation(CGPointZero, inView: view)
@@ -342,8 +342,7 @@ class DDVSlideMenuController: UIViewController {
     }
     
     private func sendAllSubViewsToBackExcept(holdView: UIView) {
-        view.bringSubviewToFront(holdView)
-        view.bringSubviewToFront(centerViewController!.view)
+        view.insertSubview(holdView, belowSubview: centerViewController.view)
     }
  
     /*
